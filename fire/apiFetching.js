deefc735e79f93060d9d0b67481bdd666bd73019
Sarah Denaci <sarahdenaci@gmail.com>
2017-09-28 12:08:35 -0400

    add user profile api

--- fire/apiFetching.js
+++ fire/apiFetching.js
@@ -1,6 +1,7 @@
 // Get a reference to the database service
 const database = firebase.database();
 
+//when we do the sample api requests on the web dev console we have to request the suggested/relevent scopes such that the ouath token yields us the desired results. we hypothesize that spotify will ask for the relevant permissions for each user and thus generate the correct token, but we should keep this scope/tokening in mind if we are not getting back the results we desire.
 //top artists
 const fetchTopArtists = () => {
   axios.get('https://api.spotify.com/v1/me/top/artists')
@@ -65,7 +66,16 @@ const fetchRecentTracks = () => {
 const spotifyUserProfile = () => {
   axios.get('https://api.spotify.com/v1/me')
     .then(res => res.data)
-    .then(data => )
+    .then(data => {
+      let user = {};
+      user.name = data.display_name
+      user.id = user.id
+      if(data.birthdate) user.birthdate = data.birthday;
+      if (data.country) user.counter = data.country;
+      if (data.email) user.email = data.email;
+      if (data.images) user.images = data.images;
+//also available: external urls, followers(href and total)
+    })
 }
 
 
