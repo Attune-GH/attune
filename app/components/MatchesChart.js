91e6786313130923d1e7fffa1dca0cbc4e854e39
Nicole Ivanov <nivanov1030@gmail.com>
2017-10-03 17:10:36 -0400

    fix: add chart style

--- app/components/MatchesChart.js
+++ app/components/MatchesChart.js
@@ -35,10 +35,34 @@ export default class MatchesChart extends Component {
         }
       ]
     }
+    const options = {
+      scaleShowGridLines: true,
+      scaleGridLineColor: 'rgba(0,0,0,.05)',
+      scaleGridLineWidth: 1,
+      scaleShowHorizontalLines: true,
+      scaleShowVerticalLines: true,
+      bezierCurve: true,
+      bezierCurveTension: 0.4,
+      pointDot: true,
+      pointDotRadius: 4,
+      pointDotStrokeWidth: 1,
+      pointHitDetectionRadius: 20,
+      datasetStroke: true,
+      datasetStrokeWidth: 2,
+      datasetFill: true,
+      legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+    }
+    
+    const styles = {
+      graphContainer: {
+        border: '1px solid black',
+        padding: '15px'
+      }
+    }
     return (
       
-      <div>
-        <Radar data={data} />
+      <div style={styles.graphContainer}>
+        <Radar data={data} options={options} />
       </div>
     )
   }
