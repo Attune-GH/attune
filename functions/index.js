a97d08d15efa3c76b6c10b70568f781c182176bc
Sarah Denaci <sarahdenaci@gmail.com>
2017-10-05 11:41:50 -0400

    add: (addmittedly a bit buggy) 2-way  matches

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const recentSongsSimScore = require('./recentTracks');
 const topArtistsSimScore = require('./topArtists');
 const topTrackSimScore = require('./topTracks');
 
-exports.findMatches = functions.database.ref('/Users/{uid}')
+exports.findMatches = functions.database.ref('/Users/{uid}/')
   .onWrite(event => {
     //we need top tracks, top artists, recent tracks, genres
     const userId = event.params.uid
@@ -25,6 +25,7 @@ exports.findMatches = functions.database.ref('/Users/{uid}')
     event.data.ref.parent.once('value', function(snapshot){
 
         const users = Object.keys(snapshot.val())
+        console.log(users)
 
         users.forEach(user => {
 
@@ -42,7 +43,14 @@ exports.findMatches = functions.database.ref('/Users/{uid}')
 
           const matchScore = (genreScore + recentSongsScore + artistsScore + tracksScore)/4
 
-          if (!matchDict[user] & matchScore !== 0.5) matchDict[user.toString()] = matchScore
+          if (!matchDict[user] & matchScore !== 0.5) {
+            console.log(`/Users/${user.toString()}/matches/matchScores/${userId}`)
+            return admin.database().ref(`/Users/${user.toString()}/matches/matchScores/${userId}`).set(matchScore)
+            .then(valy => {
+              console.log(`/Users/${userId}/matches/matchScores/${user.toString()}`)
+              return admin.database().ref(`/Users/${userId}/matches/matchScores/${user.toString()}`).set(matchScore)
+            })
+          }
 
           if (matchScore === 0.5) {
             uid = user
@@ -53,10 +61,10 @@ exports.findMatches = functions.database.ref('/Users/{uid}')
 
         return matchDict
     })
-    .then(u => {
-      console.log('where im writing to', `/Users/${uid}/matches`)
-      return admin.database().ref(`/Users/${uid}/matches`).update({matchScores: matchDict})
-    })
+    // .then(u => {
+    //   console.log('where im writing to', `/Users/${uid}/matches`)
+    //   return admin.database().ref(`/Users/${uid}/matches`).update({matchScores: matchDict})
+    // })
     .then(result => console.log('new matches written'))
 
 
