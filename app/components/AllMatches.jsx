53d8598da32d3e6a154a4771db508bd19b4ee444
Sarah Denaci <sarahdenaci@gmail.com>
2017-10-06 14:06:31 -0400

    add: percentage and sorted list

--- app/components/AllMatches.jsx
+++ app/components/AllMatches.jsx
@@ -6,6 +6,7 @@ import OneMatch from './OneMatch'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { getMatches } from 'APP/fire/refs'
+import store, {constantlyUpdateUser} from '../store'
 
 class SimpleSlider extends React.Component {
 
@@ -23,6 +24,9 @@ class SimpleSlider extends React.Component {
   }
 
   componentDidMount() {
+    store.dispatch(constantlyUpdateUser())
+    const user = this.props.user
+    console.log(user)
     const uid = this.props.user.uid
     console.log('uid', uid)
     getMatches(uid).then(matches => this.setState({ matches }))
@@ -42,15 +46,32 @@ class SimpleSlider extends React.Component {
     const matchNames = Object.keys(matches)
     console.log('matches obj', matches)
     console.log('matches keys', matchNames)
+    // const matchesSorted = Object.keys(matches).sort(function(a,b){return list[b]-list[a]})
+    var sortable = [];
+    for (var person in matches) {
+    sortable.push([person, matches[person]]);
+    }
+
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    console.log('sorted', sortable)
+    var matchesSorted ={}
+
+    for (let i = 0; i <= sortable.length-1; i++) {
+      matchesSorted[sortable[i][0]] = sortable[i][1]
+    }
+    console.log('matches sorted', matchesSorted)
+    const matchesKeys = Object.keys(matchesSorted)
+    console.log(matchesKeys)
+
     return (
       <div >
-        <h1>Your Matches!!!!</h1>
+        <h1>Your Matches, sorted by compatibility!!!!</h1>
 
-        {matches && matchNames.map(match =>
-          <h1>{match.slice(13)}</h1>
+        {matchesKeys && matchesKeys.map(match =>
+          <h3>You have a {(matchesSorted[match])*200}% match with {match.slice(13)}</h3>
         )}
-
-        <div>Swipe Through</div>
       </div>
     )
   }
@@ -59,6 +80,7 @@ class SimpleSlider extends React.Component {
 //<div><OneMatch match={match}/></div>
 //        <Slider {...settings} className="container">
 //        </Slider>
+//<div>Swipe Through</div>
 const mapStateToProps = (state) => {
   return {
     user: state.user
