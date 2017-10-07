677e9e3c8095e293d08b1733868a381690ea443c
Nicole Ivanov <nivanov1030@gmail.com>
2017-10-07 19:25:39 -0400

    fix: trying to render matchesChart

--- app/components/MatchesChart.js
+++ app/components/MatchesChart.js
@@ -12,7 +12,7 @@ class MatchesChart extends Component {
   }
 
   componentDidMount() {
-    store.dispatch(constantlyUpdateUser())
+    // store.dispatch(constantlyUpdateUser())
     const user = this.props.user
     console.log(user)
     const uid = this.props.user.uid
@@ -21,9 +21,84 @@ class MatchesChart extends Component {
   }
 
   render() {  
+    const options = {
+      scale: {
+        display: true,
+        gridLines: {
+          color: 'rgba(255,255,255,0.9)'
+        },
+        ticks: {
+          backdropColor: 'rgba(0,0,0,0.9)'
+       }
+      }
+      
+    }
+    
+
+    const styles = {
+      graphContainer: {
+        border: '1px solid white',
+        padding: '15px'
+      }
+    }
+    const matches = this.state.matches
+    const matchNames = Object.keys(matches)
+    console.log('matches obj', matches)
+    console.log('matches keys', matchNames)
+    var sortable = [];
+    for (var person in matches) {
+    sortable.push([person, matches[person]]);
+    }
+
+  let sortedMatches = sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    
+    console.log('sorted', sortedMatches)
+    
+    if(sortedMatches.length > 5) sortedMatches = sortedMatches.slice(0, 5)
+
+    const translucentColors = ['rgba(179,181,198,0.2)', 'rgba(255,99,132,0.2)', 'rgb(201,81,232,0.2)', 'rgb(86,170,234,0.2)', 'rgb(92,224,138,0.2)']
+
+    const opaqueColors = ['rgba(179,181,198,1)', 'rgba(255,99,132,1)', 'rgb(201,81,232,1)', 'rgb(86,170,234,1)', 'rgb(92,224,138,1)']
+
+    const dataset$ = sortedMatches.map((person, ind) => {
+      return {
+        label: person[0],
+        backgroundColor: translucentColors[ind],
+        borderColor: opaqueColors[ind],
+        pointBackgroundColor: opaqueColors[ind],
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: opaqueColors[ind],
+        data: [person[1], person[1], person[1], person[1]]
+      }
+    })
+
     const data = {
-      labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
-      datasets: [
+      labels: ['Top Artists', 'Top Tracks', 'Recent Tracks', 'Genres'],
+      datasets: dataset$
+    }
+
+    return (
+      
+      <div style={styles.graphContainer}>
+        <Radar data={data} options={options} />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(MatchesChart)
+
+/*
+datasets: [
         {
           label: 'My First dataset',
           backgroundColor: 'rgba(179,181,198,0.2)',
@@ -45,55 +120,4 @@ class MatchesChart extends Component {
           data: [28, 48, 40, 19, 96, 27, 100]
         }
       ]
-    }
-    const options = {
-      scale: {
-        display: true,
-        gridLines: {
-          color: 'rgba(255,255,255,0.9)'
-        },
-        ticks: {
-          backdropColor: 'rgba(0,0,0,0.9)'
-       }
-      }
-      
-    }
-    
-    // scaleShowGridLines: true,
-    // scaleGridLineColor: 'rgba(255,255,255,0.9)',
-    // scaleGridLineWidth: 1,
-    // scaleShowHorizontalLines: true,
-    // scaleShowVerticalLines: true,
-    // bezierCurve: true,
-    // bezierCurveTension: 0.4,
-    // pointDot: true,
-    // pointDotRadius: 4,
-    // pointDotStrokeWidth: 1,
-    // pointHitDetectionRadius: 20,
-    // datasetStroke: true,
-    // datasetStrokeWidth: 2,
-    // datasetFill: true,
-    // legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-
-    const styles = {
-      graphContainer: {
-        border: '1px solid white',
-        padding: '15px'
-      }
-    }
-    return (
-      
-      <div style={styles.graphContainer}>
-        <Radar data={data} options={options} />
-      </div>
-    )
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    user: state.user
-  }
-}
-
-export default connect(mapStateToProps)(MatchesChart)
+      */
