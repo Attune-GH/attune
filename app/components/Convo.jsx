145c8cf5e35e5658c7ac01abadd887fc732baaec
Kimberly Ton <kimberlyton@gmail.com>
2017-10-07 19:25:39 -0400

    stuffz

--- app/components/Convo.jsx
+++ app/components/Convo.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import store from '../store/index'
 const auth = firebase.auth()
 import { fetchConvoIdThunk } from '../store/convo'
-
+import { addMessage } from '../store/messages'
 let friendUid = "spotify:user:jpvelez"
 
 class Convo extends Component {
@@ -40,64 +40,44 @@ class Convo extends Component {
 
   handleSubmit(event){
     event.preventDefault()
-    // const userId = this.state.currentUser.uid
 
-    //Creates a new Convo Key for the convo *IF NO CONVO EXISTS WITH THE USER*
-    
-      //writes to Users/UserId/ConvoIds to add a record of that conversation
-      
-       
-      // firebase.database().ref(`Users/${userId}/ConvoIds`).push({"BobUID": newConvoKey})
-
-      // var updates = {};
-      // updates['/Convos/' + newConvoKey] = {}
-      // firebase.database().ref().update(updates)
     
     //Write to Convos table at that Convo Key
-    console.log("I HAVE BEEN CLICKED")
-    firebase.database().ref(`Convos/${this.props.convoId}`).push({from: this.props.user.uid, content: this.state.enteredMessage})
+    const messageObject = {from: this.props.user.uid, content: this.state.enteredMessage}
+
+    // firebase.database().ref(`Convos/${this.props.convoId}`).push(messageObject)
+    const messageKey = firebase.database().ref(`Convos/${this.props.convoId}`).push(messageObject).key
+    console.log("MESSAGE KEY!", messageKey)
+
     firebase.database().ref(`Convos/${this.props.convoId}`).on("child_added", ()=> {
-      console.log("SENT A MESSAGE IM IN THE CALLBACK")
-      console.log("this.props.messages MEOW!!!!!!", this.props.messages)
+      this.props.dispatchAddMessage({messageObject})
     })
-    // var updates = {};
-    // updates[`Users/${userId}/ConvoIds`] = newConvoKey
-
-  
-    // firebase.database().ref().update(updates)
-    // firebase.database().ref(`Users/${userId}/ConvoIds`).update(newConvoKey)
+      
     
-    // firebase.database().ref('Convos').push({author: this.state.currentUser.uid, content: this.state.enteredMessage})
-
-    // const newPostKey = firebase.database().ref().child('Convos').push().key;
-    // // firebase.database().ref(`Convos`).set(this.state.enteredMessage)
-    // var updates = {};
-    // updates['/Convos/' + newPostKey] = this.state.enteredMessage
-    // firebase.database().ref().update(updates)
-    this.setState({enteredMessage: ''})
+    // this.setState({enteredMessage: ''})
   }
 
   render(){
-    console.log("THE STATE IN HERE", this.state)
-    // console.log("this.state.currentUser.uid", this.state.currentUser.uid)
-    console.log("THIS PROPS", this.props)
-
-    const messageArray = Object.entries(this.props.messages)
+    if(this.props.messages) {
+      const messageArray = Object.entries(this.props.messages)
+        {messageArray && messageArray.map(message=>{
+          
+            return 
+              <div>
+                <h2>{message[1].from}: </h2>
+                <h3>{message[1].content}</h3>
+                <br/>
+              </div>
+            }
+          )}
+    }
 
     return(
     <div className = "container">
       <h3> Scintillating Conversation Between Two Interesting People Who Love The Same Music </h3>
         <br/>
-
-        {messageArray.map(message=> {
-        {console.log("message1content", message[1].content)}
-        {/* <h3>{message[1].content}</h3> */}
-        }
-        )}
-
-        {this.props.user.displayName}: YOURE SO CHARMING
-        <br/>
-        {friendUid.slice(13)}: NO YOURE SO CHARMING
+        
+  
         <div >
         <form onSubmit = {this.handleSubmit}>
             <label>{this.props.user.displayName}:</label> 
@@ -125,6 +105,9 @@ const mapDispatchToProps = (dispatch)=> {
   return {
     initializeConvo: function(uid, friendUid){
       dispatch(fetchConvoIdThunk(uid, friendUid))
+    }, 
+    dispatchAddMessage: function(message){
+      dispatch(addMessage(message))
     }
   }
 }
--- app/store/messages.js
+++ app/store/messages.js
@@ -2,9 +2,11 @@ import firebase from 'APP/fire'
 
 //ACTION TYPES
 const GET_MESSAGES = 'GET_MESSAGES'
+const ADD_MESSAGE = 'ADD_MESSAGE'
 
 //ACTION CREATOR
 const getMessages  = allMessages => ({type: GET_MESSAGES, allMessages})
+export const addMessage = message => ({type: ADD_MESSAGE, message})
 
 //THUNK CREATOR
 export const getMessagesThunk = (convoKey) => {
@@ -24,6 +26,9 @@ export default function reducer(messages = [], action){
   switch(action.type){
     case GET_MESSAGES:
       return action.allMessages;
+
+    case ADD_MESSAGE: 
+      return Object.assign({}, messages, action.message)
     default: 
       return messages 
   }
