45f573cd7fbced9a3815b3f443495a9353631e9d
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-10-08 19:21:14 -0400

    mini logo on login and navbar stuck to bottom

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,11 +4,14 @@ import { Image } from 'react-bootstrap'
 //hold for logo and also  a redirect to Dashboard
 const Footer = () => {
   return (
-  <div className="container footer">
-    <div><Image src={'/img/back.svg'} style={{ height: '125%' }} /></div>
-    {<div><h3>Dashboard</h3></div>}
-    <div><Image src={'/img/forward.svg'} style={{ height: '125%' }} /></div>
-  </div>)
+    <nav>
+      <div className="container footer">
+        <div><Image src={'/img/back.svg'} style={{ height: '125%' }} /></div>
+        {<div><h3>Dashboard</h3></div>}
+        <div><Image src={'/img/forward.svg'} style={{ height: '125%' }} /></div>
+      </div>
+    </nav>
+  )
 }
 
 export default Footer
--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import firebase from 'APP/fire'
-
+import { Image } from 'react-bootstrap'
 
 // const google = new firebase.auth.GoogleAuthProvider()
 
@@ -9,5 +9,9 @@ export default ({ auth }) =>
   // signInWithPopup will try to open a login popup, and if it's blocked, it'll
   // redirect. If you prefer, you can signInWithRedirect, which always
   // redirects.
-  <button className='btn btn-primary'
-          onClick={() => window.open('/login', 'firebaseAuth', 'height=315, width=400')}>Login with Spotify</button>
+  (<div className="container profile" >
+    <Image src={'/img/Attune-11.svg'} style={{ height: '200px'}} />
+    <button className='btn btn-primary'
+      onClick={() => window.open('/login', 'firebaseAuth', 'height=315, width=400')}>Login with Spotify</button>
+  </div>
+  )
