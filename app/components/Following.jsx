3dfe16cd6805fb4fe08e1308e76573d2dc8bb13e
Sarah Denaci <sarahdenaci@gmail.com>
2017-10-10 18:16:01 -0400

    add: dashboard button and component did mount

--- app/components/Following.jsx
+++ app/components/Following.jsx
@@ -4,11 +4,13 @@ import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
 import { getFollowing } from 'APP/fire/refs'
+import FollowedPerson from './FollowedPerson'
 import firebase from 'APP/fire'
 import { withRouter } from 'react-router'
 import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 import { connect } from 'react-redux'
 import store, { constantlyUpdateUser } from '../store'
+
 
 const img = 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAfRAAAAJGY5YjFhN2Q2LTUyNjMtNDQ4OS04Mzk5LTcyMGQyM2E0MTgwOA.jpg'
 
@@ -27,23 +29,32 @@ const img = 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAfRAA
     componentWillReceiveProps(nextProps) {
       if (this.props.user.uid !== nextProps.user.uid) {
       getFollowing(nextProps.user.uid).then(following =>
-      this.setState(following))
+      this.setState({following}))
     }
-    }
+  }
 
 
     componentDidMount() {
       store.dispatch(constantlyUpdateUser())
       const uid = this.props.user.uid
+      getFollowing(this.props.user.uid).then(following =>
+      this.setState({following}))
+
     }
 
     render() {
 
       let followedPersons = Object.keys(this.state.following)
-      console.log(followedPersons)
 
       return (
-          this.state.following.length && this.state.following.m
+        <div style={{textAlign: 'center'}}>
+          <h1>Users You're Following</h1>
+          <List style={{maxWidth: '600px'}}>
+          {followedPersons.length && followedPersons.map(person => {
+            return <div key={person}><FollowedPerson person={person}/></div>
+          })}
+          </List>
+        </div>
       )
 
     }
