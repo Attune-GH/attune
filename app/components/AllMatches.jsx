74a702b733ac5d854972acfb95a18e9e7e6c2bed
Sarah Denaci <sarahdenaci@gmail.com>
2017-10-10 12:43:00 -0400

    fix: really weird doubling bug

--- app/components/AllMatches.jsx
+++ app/components/AllMatches.jsx
@@ -38,70 +38,6 @@ componentDidMount() {
 
   render() {
 
-      let matches
-      matches = (this.state.matches ? this.state.matches : [])
-      let matchNames = Object.keys(matches)
-      var sortable = [];
-      for (var person in matches) {
-        sortable.push([person, matches[person]]);
-      }
-
-      const betterArr = sortable.sort(function (a, b) {
-        return b[1] - a[1];
-      })
-
-      var settings = {
-      initialSlide: 0,
-      slickGoTo: 0,
-      dots: false,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true
-    }
-
-
-import React from 'react'
-import Slider from 'react-slick'
-import { Image } from 'react-bootstrap'
-import UserProfile from './UserProfile'
-import OneMatch from './OneMatch'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-import { getMatches } from 'APP/fire/refs'
-import firebase from 'APP/fire'
-
-import store, { constantlyUpdateUser } from '../store'
-
-class SimpleSlider extends React.Component {
-
-  constructor(props) {
-    super()
-    this.state = {
-      matches: []
-    }
-  }
-
-  //not sure i understand this function --eks
-  componentWillReceiveProps(nextProps) {
-    if (this.props.user.uid !== nextProps.user.uid) {
-      getMatches(nextProps.user.uid).then(matches =>
-      this.setState({matches}))
-    }
-  }
-
-
-componentDidMount() {
-      store.dispatch(constantlyUpdateUser())
-      const uid = this.props.user.uid
-      firebase.database().ref(`Users/${uid}/matches/matchScores`).on("child_added", ()=> {
-          getMatches(uid).then(matches => this.setState({ matches }))
-      })
-    }
-
-  render() {
-
 
       let matches
       matches = (this.state.matches ? this.state.matches : [])
@@ -147,15 +83,6 @@ componentDidMount() {
     )
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    user: state.user
-  }
-}
-
-export default withRouter(connect(mapStateToProps)(SimpleSlider))
-
 
 const mapStateToProps = (state) => {
   return {
