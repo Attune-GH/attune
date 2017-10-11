0137cf5cb09356bcd3687999f953530b5d165c58
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-10-10 21:36:41 -0400

    proposition: matches look better without that header

--- app/components/AllMatches.jsx
+++ app/components/AllMatches.jsx
@@ -70,7 +70,7 @@ componentDidMount() {
       <div>
         {betterArr.length ?
         <div>
-        <div className="container matches"><h2 style={{textAlign: 'center'}}>Your Matches</h2></div>
+        {/* <div className="container matches"><h2 style={{textAlign: 'center'}}>Your Matches</h2></div> */}
           <Slider {...settings} className="container">
             {betterArr.length === goodMatches.length && betterArr.map(match =>{
               return <div key={match[0]}><OneMatch match={match} /></div>})}
