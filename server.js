3451182e2cfc307aa0572586167dfcdb59ccc70a
Sarah Denaci <sarahdenaci@gmail.com>
2017-10-13 09:44:49 -0400

    console

--- server.js
+++ server.js
@@ -257,6 +257,6 @@ function createFirebaseAccount(uid, displayName, photoURL, birthdate, accessToke
 
 
 console.log('Listening on 1337');
-http.listen((process.env.PORT || 1337), function(){
+app.listen((process.env.PORT || 1337), function(){
   console.log('listening on 1337');
 });
